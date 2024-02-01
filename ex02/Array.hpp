9c99d3fbb3936cca3fe77c656b501f12712deea1
Tuomas Jääsalo <tuomas.jaasalo@alceon.fi>
2024-02-01 14:08:55 +0200

    Move array impls to .tpp
    + removed unnecessary nullchecks
    + added missing const qualifier

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -1,81 +1,28 @@
 #pragma once
 
-#include <cstring>
-#include <utility>
-#include <exception>
+#include <cstddef>
 
 template <typename T>
 class Array
 {
     public:
-        ~Array()
-        {
-            if (_buffer)
-            {
-                delete[] _buffer;
-            }
-        }
+        ~Array();
+        Array();
+        Array(unsigned int n);
+        Array(const Array& other);
+        Array(Array&& other) noexcept;
 
-        Array() : _buffer(nullptr), _size(0) {}
+        Array&  operator=(const Array& other);
+        Array&  operator=(Array&& other) noexcept;
+        T&      operator[](size_t index) const;
 
-        Array(unsigned int n) :_buffer(new T[n]), _size(n) {}
-
-        Array(const Array& other) :
-            _buffer(copy_buffer(other)),
-            _size(other._size) {}
-
-        Array(Array&& other) noexcept :
-            _buffer(std::exchange(other._buffer, nullptr)),
-            _size(std::exchange(other._size, 0)) {}
-
-        Array& operator=(const Array& other)
-        {
-            T* buffer;
-
-            if (*this == other)
-            {
-                return *this;
-            }
-
-            buffer = copy_buffer(other);
-            if (_buffer)
-            {
-                delete[] _buffer;
-            }
-
-            _buffer = buffer;
-            _size = other._size;
-            return *this;
-        }
-
-        Array& operator=(Array&& other) noexcept
-        {
-            _buffer = std::exchange(other._buffer, _buffer);
-            _size = std::exchange(other._size, _size);
-            return *this;
-        }
-
-        T& operator[](size_t index) const
-        {
-            if (index >= _size)
-            {
-                throw std::exception();
-            }
-            return _buffer[index];
-        }
-
-        size_t size() { return _size; }
+        size_t size() const;
 
     private:
         T*      _buffer;
         size_t  _size;
 
-        static T* copy_buffer(Array<T> other)
-        {
-            if (!other._buffer)
-            {
-                return nullptr;
-            }
-            return static_cast<T*>(std::memcpy(new T[other._size], other._buffer, other._size));
-        }
+        static T* copy_buffer(Array<T> other);
 };
+
+#include "Array.tpp"
